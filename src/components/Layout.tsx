9562a9eb3aad2b90a264232c3ce9e037a3004b04
HarukiKaneko <niku11298@gmail.com>
2022-09-06 16:40:00 +0900

    add eslint

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,8 @@
-import { useRouter } from "next/router";
-
 import { ContentsWrapper } from "./ContentWrapper";
 import { BodyWrapper } from "./BodyWrapper";
 import { TopNavbar } from "./TopNavbar";
 
 export const Layout: React.FC<{ children }> = ({ children }) => {
-  const router = useRouter();
   return (
     <BodyWrapper>
       <TopNavbar />
