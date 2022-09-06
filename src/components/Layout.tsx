fb4aea2bc6ecd1f1aa31fe686387690d72b65993
HarukiKaneko <niku11298@gmail.com>
2022-09-06 13:19:37 +0900

    remove import

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,7 @@
-import { icons } from "./atoms/Icon";
 import { useRouter } from "next/router";
 
 import { ContentsWrapper } from "./ContentWrapper";
 import { BodyWrapper } from "./BodyWrapper";
-import "react-minimal-side-navigation/lib/ReactMinimalSideNavigation.css";
-import { Button, Dropdown, Link, Navbar } from "@nextui-org/react";
 import { TopNavbar } from "./TopNavbar";
 
 export const Layout: React.FC<{ children }> = ({ children }) => {
